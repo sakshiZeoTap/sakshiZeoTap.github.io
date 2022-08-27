310dca0a14413d948bdbe8b24db0961203d92ed4
Sakshi Gaur <sakshigaur@ZT-IN-Mac-Sakshi-Gaur.local>
2022-08-27 13:58:35 +0530

    FIx

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ $("#submit").click(function( event ) {
   $('#filter-select').val("select-filter")
   $('#country-select').val("select-country")
   $('#channel-id').val("")
-  alert( "Form is Submitted" );
+  alert( "Segment Data is Submitted" );
 
 });
