ec445ef0c75910108c63adb5d9fff5c47e23dc2e
sakshiZeoTap <87962457+sakshiZeoTap@users.noreply.github.com>
2022-08-27 14:22:29 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,5 @@ $("#submit").click(function( event ) {
   $('#filter-select').val("select-filter")
   $('#country-select').val("select-country")
   $('#channel-id').val("")
-  alert( "Segment Filter is Saved.The Segments will be used for filtering the user and pushing it to the channel" );
-
+  alert("Segment Filter is Saved.The Segments will be used for filtering the user and pushing it to the channel")
 });
