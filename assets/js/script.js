bc0f941044b5738e26a0ddaa40a5721eb115482a
Sakshi Gaur <sakshigaur@ZT-IN-Mac-Sakshi-Gaur.local>
2022-08-27 14:15:50 +0530

    FIx

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,6 @@ $("#submit").click(function( event ) {
   $('#filter-select').val("select-filter")
   $('#country-select').val("select-country")
   $('#channel-id').val("")
-  alert( "Segment Data is Submitted" );
+  alert( "Segment Filter is Saved.The Segments will be used for filtering the user and pushing it to the channel" );
 
 });
